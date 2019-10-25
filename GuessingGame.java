a628c863b81711371e9da58707627b41bd2971be
Simon Hagelin <simon_hagelin@hotmail.com>
2019-10-25 16:45:44 +0200

    Added a try-catch for arguments

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,13 +1,19 @@
 public class GuessingGame {
 
     public static void main(String[] args) {
-        Guesser g;
-        if (args.length != 0) {
+        
+	try{
+	Guesser g;
+        if (args.length != 0 && args.length<3) {
             g = new Guesser(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         } else {
             g = new Guesser(0, 1000);
         }
-        g.start();
+	g.start();
+	}catch(NumberFormatException nfe){
+  System.exit(2);
+ }
+        
 
     }
 
